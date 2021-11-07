62dc8def1586bd33d4bdf2f3d7be3a95112880e8
zoreliu <zoreliu@tencent.com>
2021-11-07 17:46:35 +0800

    test

--- src/index.js
+++
@@ -1,2 +0,0 @@
-const xxx = require('../packages/log/dist/log.es')
-console.log('xxx: ', xxx);
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import log from "@ts-lernarepo/app";
+
+
+// import log from "../packages/app";
+
+log()
+
+console.log('log: ', log);
+
+
